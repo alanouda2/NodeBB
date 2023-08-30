b058393eb5b1ab673fc449e0b9807eae1f066339
alanouda2 <alanoud2@andrew.cmu.edu>
2023-08-30 13:47:03 +0300

    Fixed Translation of languages.js

--- src/languages.js
+++ src/languages.js
@@ -1,27 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -31,22 +8,26 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.listCodes = exports.get = void 0;
-const fs = __importStar(require("fs/promises"));
-const path = __importStar(require("path"));
-const plugins = __importStar(require("./plugins"));
-// Export the function for getting language data
+exports.userTimeagoCode = exports.list = exports.listCodes = exports.get = void 0;
+const promises_1 = __importDefault(require("fs/promises"));
+const path_1 = __importDefault(require("path"));
+const utils_1 = __importDefault(require("./utils"));
+const plugins_1 = __importDefault(require("./plugins"));
+const constants_1 = require("./constants");
 function get(language, namespace) {
     return __awaiter(this, void 0, void 0, function* () {
-        const languagesPath = path.join(__dirname, '../build/public/language');
-        const pathToLanguageFile = path.join(languagesPath, language, `${namespace}.json`);
+        const languagesPath = path_1.default.join(__dirname, '../build/public/language');
+        const pathToLanguageFile = path_1.default.join(languagesPath, language, `${namespace}.json`);
         if (!pathToLanguageFile.startsWith(languagesPath)) {
             throw new Error('[[error:invalid-path]]');
         }
-        const data = yield fs.readFile(pathToLanguageFile, 'utf8');
+        const data = yield promises_1.default.readFile(pathToLanguageFile, 'utf8');
         const parsed = JSON.parse(data);
-        const result = yield plugins.hooks.fire('filter:languages.get', {
+        const result = yield plugins_1.default.hooks.fire('filter:languages.get', {
             language,
             namespace,
             data: parsed,
@@ -55,27 +36,50 @@ function get(language, namespace) {
     });
 }
 exports.get = get;
-// Export the function for listing language codes
-let codeCache = null;
+let codeCache;
 function listCodes() {
     return __awaiter(this, void 0, void 0, function* () {
         if (codeCache && codeCache.length) {
             return codeCache;
         }
-        try {
-            const languagesPath = path.join(__dirname, '../build/public/language');
-            const file = yield fs.readFile(path.join(languagesPath, 'metadata.json'), 'utf8');
-            const parsed = JSON.parse(file);
-            codeCache = parsed.languages;
-            return parsed.languages;
-        }
-        catch (err) {
-            const customError = err;
-            if (customError.code === 'ENOENT') {
-                return [];
-            }
-            throw err;
-        }
+        const languagesPath = path_1.default.join(__dirname, '../build/public/language');
+        const file = yield promises_1.default.readFile(path_1.default.join(languagesPath, 'metadata.json'), 'utf8');
+        const parsed = JSON.parse(file);
+        codeCache = parsed.languages;
+        return parsed.languages;
     });
 }
 exports.listCodes = listCodes;
+let listCache;
+function list() {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (listCache && listCache.length) {
+            return listCache;
+        }
+        const codes = yield listCodes();
+        let languages = yield Promise.all(codes.map((folder) => __awaiter(this, void 0, void 0, function* () {
+            const languagesPath = path_1.default.join(__dirname, '../build/public/language');
+            const configPath = path_1.default.join(languagesPath, folder, 'language.json');
+            const file = yield promises_1.default.readFile(configPath, 'utf8');
+            const lang = JSON.parse(file);
+            return lang;
+        })));
+        languages = languages.filter(lang => lang && lang.code && lang.name && lang.dir);
+        listCache = languages;
+        return languages;
+    });
+}
+exports.list = list;
+function userTimeagoCode(userLang) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const files = yield promises_1.default.readdir(path_1.default.join(constants_1.paths.nodeModules, '/timeago/locales'));
+        const timeagoCodes = files.filter(f => f.startsWith('jquery.timeago')).map(f => f.split('.')[2]);
+        const languageCodes = yield listCodes();
+        const timeagoCode = utils_1.default.userLangToTimeagoCode(userLang);
+        if (languageCodes.includes(userLang) && timeagoCodes.includes(timeagoCode)) {
+            return timeagoCode;
+        }
+        return '';
+    });
+}
+exports.userTimeagoCode = userTimeagoCode;
--- src/languages.ts
+++ src/languages.ts
@@ -1,48 +1,88 @@
-import * as fs from 'fs/promises';
-import * as path from 'path';
-import * as plugins from './plugins';
+import fs from 'fs/promises';
+import path from 'path';
+import utils from './utils';
+import plugins from './plugins';
+import { paths } from './constants';
 
-// Define a type for the specific error you're handling
-interface CustomError extends NodeJS.ErrnoException {
-    code: string;
+interface Language {
+    code: string,
+    name: string,
+    dir: string
 }
 
-// Export the function for getting language data
-export async function get(language: string, namespace: string): Promise<Record<string, string>> {
+interface ParseResult {
+    languages: string[]
+}
+
+interface Result {
+    data: Language[]
+}
+
+
+export async function get(language: string, namespace: string) {
     const languagesPath = path.join(__dirname, '../build/public/language');
     const pathToLanguageFile = path.join(languagesPath, language, `${namespace}.json`);
+
     if (!pathToLanguageFile.startsWith(languagesPath)) {
         throw new Error('[[error:invalid-path]]');
     }
 
     const data = await fs.readFile(pathToLanguageFile, 'utf8');
-    const parsed = JSON.parse(data) as Record<string, string>;
-    const result = await plugins.hooks.fire('filter:languages.get', {
+    const parsed: ParseResult = JSON.parse(data) as ParseResult;
+
+    const result: Result = await plugins.hooks.fire('filter:languages.get', {
         language,
         namespace,
         data: parsed,
-    }) as { data: Record<string, string> };
+    }) as Result;
 
     return result.data;
 }
 
-// Export the function for listing language codes
-let codeCache: string[] | null = null;
-export async function listCodes(): Promise<string[]> {
+let codeCache: string[];
+
+export async function listCodes() {
     if (codeCache && codeCache.length) {
         return codeCache;
     }
-    try {
-        const languagesPath = path.join(__dirname, '../build/public/language');
-        const file = await fs.readFile(path.join(languagesPath, 'metadata.json'), 'utf8');
-        const parsed = JSON.parse(file) as { languages: string[] };
-        codeCache = parsed.languages;
-        return parsed.languages;
-    } catch (err) {
-        const customError = err as CustomError;
-        if (customError.code === 'ENOENT') {
-            return [];
-        }
-        throw err;
+    const languagesPath = path.join(__dirname, '../build/public/language');
+    const file = await fs.readFile(path.join(languagesPath, 'metadata.json'), 'utf8');
+    const parsed: ParseResult = JSON.parse(file) as ParseResult;
+    codeCache = parsed.languages;
+    return parsed.languages;
+}
+
+let listCache: Language[];
+
+export async function list() {
+    if (listCache && listCache.length) {
+        return listCache;
     }
+
+    const codes = await listCodes();
+
+    let languages: Language[] = await Promise.all(codes.map(async (folder) => {
+        const languagesPath = path.join(__dirname, '../build/public/language');
+        const configPath = path.join(languagesPath, folder, 'language.json');
+        const file = await fs.readFile(configPath, 'utf8');
+        const lang: Language = JSON.parse(file) as Language;
+        return lang;
+    }));
+
+    languages = languages.filter(lang => lang && lang.code && lang.name && lang.dir);
+
+    listCache = languages;
+    return languages;
+}
+
+export async function userTimeagoCode(userLang: string) {
+    const files = await fs.readdir(path.join(paths.nodeModules, '/timeago/locales'));
+    const timeagoCodes = files.filter(f => f.startsWith('jquery.timeago')).map(f => f.split('.')[2]);
+    const languageCodes = await listCodes();
+    const timeagoCode: string = utils.userLangToTimeagoCode(userLang) as string;
+
+    if (languageCodes.includes(userLang) && timeagoCodes.includes(timeagoCode)) {
+        return timeagoCode;
+    }
+    return '';
 }
